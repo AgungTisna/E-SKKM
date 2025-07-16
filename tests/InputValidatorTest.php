ef655458dc36f6965ec98816b4e34828f2ec5e96
AgungTisna <gusdwik34@gmail.com>
2025-07-16 13:36:42 +0800

    Initial commit of e-skkm project

--- tests/InputValidatorTest.php
+++ tests/InputValidatorTest.php
@@ -1,6 +1,7 @@
 <?php
-use vendor\PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../inputValidator.php';
 
 class InputValidatorTest extends TestCase
